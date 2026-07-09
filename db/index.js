c50b4ae3ac6077a2dc8cdc7ce5b753d50d77bf01
Denis <siusin91denis@mail.com>
2026-07-09 14:43:52 +0300

    move db to /app/data

--- db/index.js
+++ db/index.js
@@ -1,7 +1,13 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 
-const db = new Database(path.join(__dirname, '..', 'data.db'));
+const dataDir = '/app/data';
+if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir, { recursive: true });
+}
+
+const db = new Database(path.join(dataDir, 'data.db'));
 
 db.exec(`
     CREATE TABLE IF NOT EXISTS users (
